1ba9dd1012319e6fbfe871b850155d8ca905e6f9
Dima Telechkun <telechkun93@gmail.com>
2022-12-30 00:45:55 +0200

    project update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,38 @@
 import { Component } from 'react';
+import { Statistics } from './Statistics/Statictics';
+import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
+import { Section } from './Section/Section';
+// import { Notification } from './Notification/Notification';
 
 export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  // countTotalFeedback() { }
+
+  // countPositiveFeedbackPercentage() { }
+
   render() {
+    const { good, neutral, bad } = this.state;
     return (
-      <div
-        style={{
-          height: '100vh',
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          fontSize: 40,
-          color: '#010101',
-        }}
-      >
-        React homework template
-      </div>
+      <>
+        <Section title="Statistics">
+          <FeedbackOptions options={0} onLeaveFeedback={0} />
+        </Section>
+        {/* <Notification message="There is no feedback" /> */}
+        <Section title="Please leave feedback">
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={0}
+            positivePercentage={0}
+          />
+        </Section>
+      </>
     );
   }
 }
---
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -0,0 +1,9 @@
+export const FeedbackOptions = () => {
+  return (
+    <div>
+      <button>Good</button>
+      <button>Neutral</button>
+      <button>Bad</button>
+    </div>
+  );
+};
